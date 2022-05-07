2c6838cf3d4ba76ff07ced1aae239ce06e09e1bd
yuuki1036 <yuuki1036@gmail.com>
2022-05-07 15:26:08 +0900

    devide component

---
+++ vue-sample/src/components/Tweet.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import TweetPostForm from './TweetPostForm.vue';
+import TweetList from './TweetList.vue';
+
+const tweets = ref([
+  {
+    id: 0,
+    desc: "hello world"
+  },
+  {
+    id: 1,
+    desc: "yeeaaa"
+  },
+])
+
+const postTweet = (desc: string) => {
+  const tweet = {id: Math.random(), desc}
+  tweets.value.push(tweet)
+}
+
+const deleteTweet = (id: number) => {
+  tweets.value = tweets.value.filter((t) => t.id !== id)
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>tweeter</h1>
+    <TweetPostForm @post-tweet="postTweet"/>
+    <TweetList :tweets="tweets" @delete-tweet="deleteTweet"/>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ vue-sample/src/components/TweetPostForm.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const inputtingDesc = ref<string>('')
+const emit = defineEmits(["post-tweet"])
+const postTweet = () => {
+  emit("post-tweet", inputtingDesc.value)  
+  inputtingDesc.value = ''
+}
+
+
+
+</script>
+
+<template>
+  <div class="form-container">
+    <input v-model="inputtingDesc"/>
+    <button class="save-button" @click="postTweet()">post</button>
+  </div>
+</template>
+
+<style scoped>
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+  padding: 24px 0;
+  width: 60%;
+  margin-bottom: 12px;
+  border-radius: 4px;
+}
+.save-button {
+  color: #fff;
+  font-weight: bold;
+  background-color: #68c9c9;
+  border-radius: 2px;
+  border:none;
+  width: 60px;
+  height: 22px;
+}
+input {
+  margin-bottom: 8px;
+}
+</style>
